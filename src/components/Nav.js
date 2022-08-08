3b0f44af2020f7e6add6b6c232ecca849928fe2f
brianchoi93 <brianchoi93@gmail.com>
2022-08-07 21:06:12 -0700

    Render links to nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav(props) {
   return (
     <div>
-      This is the nav bar
-      
+      <Link to = "/">Home</Link>
+      <Link to = "/aboutme">About Me</Link>
+      <Link to = "/projects">Projects</Link>
+      <Link to = "/skills">Skills</Link>
+      <Link to = "/contactme">Contact Me</Link>
     </div>
   );
 }
