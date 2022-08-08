f4da88a2521ddaa076ad962d021c4536cb6cf496
brianchoi93 <brianchoi93@gmail.com>
2022-08-07 21:06:35 -0700

    Input routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom"
 import './App.css';
 import Nav from "./components/Nav";
+import HomePage from "./components/HomePage";
+import AboutMe from "./components/AboutMe";
+import Project from "./components/Project";
+import Skills from "./components/Skills";
+import ContactMe from "./components/ContactMe";
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       </nav>
       <main>
         <Routes>
+          <Route path = "/" element = { <HomePage/> } />
+          <Route path = "/aboutme" element= { <AboutMe/> } />
+          <Route path = "/projects" element = { <Project/> } />
+          <Route path = "/skills" element = { <Skills/> } />
+          <Route path = "/contactme" element = { <ContactMe /> } />
         </Routes>
       </main>
     </div>
